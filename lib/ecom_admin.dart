2a3c9af700fbb534d5f9b6371967d38464daf284
Anurag Gupta <anuballia01@gmail.com>
2025-05-24 17:06:10 +0530

    setup

---
+++ lib/ecom_admin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EcomAdmin extends StatelessWidget {
+  const EcomAdmin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ecom_admin/ecom_admin.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcomAdmin());
+}
