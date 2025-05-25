a0aab5f58d71a14b7e7f8cfb300bd1bde0877bd5
Anurag Gupta <anuballia01@gmail.com>
2025-05-25 17:31:01 +0530

    firebase-core-setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:ecom_admin/ecom_admin.dart';
+import 'package:ecom_admin/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const EcomAdmin());
 }
